7d4c5a97072bfb5bfdf752ab588263454096554a
My Name <sasukee9625@gmail.com>
2019-08-17 02:13:08 +0500

    --prepare

--- src/main/java/com/diser/controller/MessageController.java
+++ src/main/java/com/diser/controller/MessageController.java
@@ -1,23 +1,37 @@
 package com.diser.controller;
 
 import com.diser.dto.EventType;
+import com.diser.dto.MetaDto;
 import com.diser.dto.ObjectType;
 import com.diser.entity.Message;
 import com.diser.entity.Views;
 import com.diser.repository.MessageRepository;
 import com.diser.util.WsSender;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("main")
 public class MessageController {
+    private static String URL_PATTERN = "https?:\\/\\/?[\\w\\d\\._\\-%\\/\\?=&#]+";
+
+    private static String IMAGE_PATTERN = "\\.(jpeg|jpg|gif|png)$";
+
+    private static Pattern URL_REGEX = Pattern.compile(URL_PATTERN, Pattern.CASE_INSENSITIVE);
+    private static Pattern IMAGE_REGEX = Pattern.compile(IMAGE_PATTERN, Pattern.CASE_INSENSITIVE);
 
     private final MessageRepository messageRepository;
     private final BiConsumer<EventType, Message> wsSender;
@@ -41,8 +55,9 @@ public class MessageController {
     }
 
     @PostMapping
-    public Message create(@RequestBody Message message) {
+    public Message create(@RequestBody Message message) throws IOException {
         message.setCreationDate(LocalDateTime.now());
+        fillMeta(message);
         Message createMessage = messageRepository.save(message);
         wsSender.accept(EventType.CREATE, createMessage);
         return createMessage;
@@ -52,8 +67,9 @@ public class MessageController {
     public Message update(
             @PathVariable("id") Message messageFromDb,
             @RequestBody Message message
-    ) {
+    ) throws IOException {
         BeanUtils.copyProperties(message, messageFromDb, "id");
+        fillMeta(messageFromDb);
         Message updateMessage = messageRepository.save(messageFromDb);
         wsSender.accept(EventType.UPDATE, updateMessage);
         return updateMessage;
@@ -63,5 +79,46 @@ public class MessageController {
     public void delete(@PathVariable("id") Message message) {
         messageRepository.delete(message);
         wsSender.accept(EventType.REMOVE, message);
+    }
+
+    private void fillMeta(Message message) throws IOException {
+        String text = message.getText();
+
+        Matcher matcher = URL_REGEX.matcher(text);
+
+        if(matcher.find()) {
+            String url = text.substring(matcher.start(), matcher.end());
+
+            matcher = IMAGE_REGEX.matcher(url);
+            message.setLink(url);
+
+            if (matcher.find()) {
+                message.setLinkCover(url);
+            } else if (!url.contains("youtu")) {
+                MetaDto meta = getMeta(url);
+
+                message.setLinkCover(meta.getCover());
+                message.setLinkTitle(meta.getTitle());
+                message.setLinkDescription(meta.getDescription());
+            }
+        }
+    }
+
+    private MetaDto getMeta(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+
+        Elements title = document.select("meta[name$=title], meta[property$=title]");
+        Elements description = document.select("meta[name$=description], meta[property$=description]");
+        Elements cover = document.select("meta[name$=image], meta[property$=image]");
+
+        return new MetaDto(
+                getContent(title.first()),
+                getContent(description.first()),
+                getContent(cover.first())
+        );
+    }
+
+    private String getContent(Element element) {
+        return element == null ? "" : element.attr("content");
     }
 }
